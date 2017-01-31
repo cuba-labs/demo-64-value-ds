dbf38b0beb71ec3e0c60d6e565f31bd4696bc035
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-01-31 12:54:29 +0400

    Upgrade to RC1

--- modules/web/src/com/company/demo/web/valueds/ValueDsDemo.java
+++ modules/web/src/com/company/demo/web/valueds/ValueDsDemo.java
@@ -1,9 +1,13 @@
 package com.company.demo.web.valueds;
 
+import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.gui.components.AbstractWindow;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.data.impl.DatasourceImplementation;
 
 public class ValueDsDemo extends AbstractWindow {
 
     public void refresh() {
+        getDsContext().getNN("salesDs").refresh();
     }
 }
